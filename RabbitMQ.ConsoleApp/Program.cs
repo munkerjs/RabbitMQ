83834053314261968c70e6b5ffbaee4c05d470a8
Münker Erkadam <munkererkadam@gmail.com>
2024-02-29 22:08:15 +0300

    RabbitMQ için temel düzeyde HelloWorld uygulaması

--- RabbitMQ.ConsoleApp/Program.cs
+++ RabbitMQ.ConsoleApp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        IConfiguration configuration = builder.Build();
+
+        string rabbitMqConnectionString = configuration.GetConnectionString("RabbitMQ") ?? "";
+
+        // Bilgilerimizi tanımlayalım
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri(rabbitMqConnectionString);
+
+        // RabbitMQ için bağlantı açalım
+        using var connection = factory.CreateConnection();
+
+        // Bağlantı Tüneli - Kanalı Oluşturalım ve RabbitMQ ya bağlanalım.
+        var channel = connection.CreateModel();
+
+        // Mesajların boşa düşmemesi için önce bir kuyruk oluşturalım.
+        string queueName = "hello-queue";
+        channel.QueueDeclare(queueName, true, false, false);
+
+        // Mesajımızı Oluşturalım.
+        string message = "Hello World!";
+
+        // RabbitMQ'ya verileri iletirken Byte dizisi şeklinde iletmekteyiz. PDF, Excel veya Image bile iletebilirsin.
+        var messageBody = Encoding.UTF8.GetBytes(message);
+
+        // Artık Mesajımızı Kuyruğa Ekleyelim.
+        channel.BasicPublish(string.Empty, queueName, null, messageBody);
+
+        Console.WriteLine("Mesajınızı Gönderildi.");
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQ.Subsriber/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        IConfiguration configuration = builder.Build();
+
+        string rabbitMqConnectionString = configuration.GetConnectionString("RabbitMQ") ?? "";
+
+        // Bilgilerimizi tanımlayalım
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri(rabbitMqConnectionString);
+
+        // RabbitMQ için bağlantı açalım
+        using var connection = factory.CreateConnection();
+
+        // Bağlantı Tüneli - Kanalı Oluşturalım ve RabbitMQ ya bağlanalım.
+        var channel = connection.CreateModel();
+
+        // Tüneldeki verileri okumak için tüketici oluşturalım
+        var consumer = new EventingBasicConsumer(channel);
+
+        // Tüketicinin erişeceği, veriyi okuyacağı kuyruk adı
+        string queueName = "hello-queue";
+
+        // [EĞİTİM AMAÇLI SEÇİLİ] 2. değer true yapılırsa, mesaj doğruda işlense yanlış da işlense kuyruktan siler.
+        // [GERÇEK PROJELERDE GENELDE KULLANIRIZ] 2. değer false yapılırsa, mesaj doğru işlenirse ben sana daha sonra silmen için bildireceğim gibi anlayabiliriz.
+        channel.BasicConsume(queueName, true, consumer);
+
+        // Subsriber'a mesaj geldiğinde bu event otomatik tetiklenecek.
+        consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine($"Gelen Mesaj: {message}");
+        };
+
+        Console.ReadLine();
+    }
+}
